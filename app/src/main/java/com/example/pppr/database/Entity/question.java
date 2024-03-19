f052a15e49fdef560e4be5138ff43673760937f6
azod002 <artemian08@gmail.com>
2024-03-19 22:30:25 +0300

    1

---
+++ app/src/main/java/com/example/pppr/database/Entity/question.java
@@ -0,0 +1,22 @@
+package com.example.pppr.database.Entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "question")
+public class question {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "authorId")
+    private int authorId;
+
+    @ColumnInfo(name = "content")
+    private int content;
+
+    @ColumnInfo(name = "title")
+    private int title;
+
+}
